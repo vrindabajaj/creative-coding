8f35e48691ba0656920545bfb0c5c61b7ab22275
Vrinda <vrinda.bajaj@gmail.com>
2024-08-09 10:13:46 +0100

    Animate circles

--- 02-Rainbow-Circles/circle.js
+++ 02-Rainbow-Circles/circle.js
@@ -3,14 +3,20 @@ class Circle {
         this.radius = 100;
         this.angle = angle;
         this.size = 100;
+        this.direction = -1;
     }
 
     update(){
         this.x = this.radius*cos(this.angle);
         this.y = this.radius*sin(this.angle);
+
+        if(this.radius < 0 || this.radius > 100){
+            this.direction = this.direction * -1;
+        }
+        this.radius = this.radius + this.direction;
     }
     display(){
         ellipse(this.x, this.y, this.size, this.size);
-        line(0, 0, this.x, this.y)
+        //line(0, 0, this.x, this.y)
     }
 }
