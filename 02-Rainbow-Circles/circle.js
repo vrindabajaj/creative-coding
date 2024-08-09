686cd7b326b7bb37f673555d2c58eac884d755f9
Vrinda <vrinda.bajaj@gmail.com>
2024-08-09 10:05:43 +0100

    Draw circles

--- 02-Rainbow-Circles/circle.js
+++ 02-Rainbow-Circles/circle.js
@@ -1,7 +1,8 @@
 class Circle {
-    constructor(){
+    constructor(angle){
         this.radius = 100;
-        this.angle = 0;
+        this.angle = angle;
+        this.size = 100;
     }
 
     update(){
@@ -9,6 +10,7 @@ class Circle {
         this.y = this.radius*sin(this.angle);
     }
     display(){
-
+        ellipse(this.x, this.y, this.size, this.size);
+        line(0, 0, this.x, this.y)
     }
 }
--- 02-Rainbow-Circles/sketch.js
+++ 02-Rainbow-Circles/sketch.js
@@ -1,7 +1,22 @@
+let circles = [];
+let count = 12;
+let angles = [];
+
 function setup() {
   createCanvas(400, 400);
+
+  for(let i = 0; i < count; i++){
+    angles[i] = (TWO_PI/count) * i;
+    circles[i] = new Circle(angles[i]);
+  }
 }
 
 function draw() {
   background(220);
+  translate(width/2, height/2);
+
+  for(let i = 0; i < count; i++){
+    circles[i].update();
+    circles[i].display();
+  }
 }
