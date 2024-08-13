aa9efc25ffd6183b6f37c97cc2df6b61d28cf542
Vrinda <vrinda.bajaj@gmail.com>
2024-08-13 15:30:46 +0100

    Rotate inner stars and connect with lines

--- 03-Moving-Stars/sketch.js
+++ 03-Moving-Stars/sketch.js
@@ -5,15 +5,16 @@ let outerAngles = [];
 let inner = []
 let innerNum = 3;
 let innerAngles = [];
+let delta = 0.03;
 
 function setup() {
   createCanvas(400, 400);
   
-  for(i = 0; i < outerNum; i++){
+  for(let i = 0; i < outerNum; i++){
     outerAngles[i] = (TWO_PI/outerNum * i);
     outer[i] = new Star(outerAngles[i], false);
   }
-  for(i = 0; i < innerNum; i++){
+  for(let i = 0; i < innerNum; i++){
     innerAngles[i] = (TWO_PI/innerNum * i);
     inner[i] = new Star(innerAngles[i], true);
   }
@@ -23,12 +24,19 @@ function draw() {
   background(220);
   translate(width/2, height/2);
 
-  for(i = 0; i < outerNum; i++){
+  for(let i = 0; i < outerNum; i++){
     outer[i].update();
     outer[i].display();
   }
-  for(i = 0; i < innerNum; i++){
+  for(let i = 0; i < innerNum; i++){
+    inner[i].angle += delta;
     inner[i].update();
     inner[i].display();
+  }
+
+  for(let i = 0; i < outerNum; i++){
+    for(let j = 0; j < innerNum; j++){
+      line(outer[i].x, outer[i].y, inner[j].x, inner[j].y);
+    }
   }
 }
