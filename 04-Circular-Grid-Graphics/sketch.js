c32e3fff5306f0a785a1c51891f7c4a1d5e7b0d1
Vrinda <vrinda.bajaj@gmail.com>
2024-08-13 20:33:24 +0100

    Draw circles

--- 04-Circular-Grid-Graphics/sketch.js
+++ 04-Circular-Grid-Graphics/sketch.js
@@ -1,7 +1,19 @@
+let cols, rows;
+let spacing = 50;
+
 function setup() {
   createCanvas(400, 400);
+  cols = width/spacing;
+  rows = height/spacing;
 }
 
 function draw() {
   background(220);
+  translate(spacing/2, spacing/2);
+
+  for(let i = 0; i < cols; i++){
+    for(let j = 0; j < rows; j++){
+      ellipse(i*spacing, j*spacing, spacing, spacing);
+    }
+  }
 }
