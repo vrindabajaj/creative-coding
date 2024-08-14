000ca47e2337086649059472ab51ad94908f687c
Vrinda <vrinda.bajaj@gmail.com>
2024-08-14 16:08:49 +0100

    Create 2D noise grid

--- 08-Disco-Grid/sketch.js
+++ 08-Disco-Grid/sketch.js
@@ -1,7 +1,36 @@
+let sizes = [];
+let cols; let rows;
+let size = 50;
+let xOff = 0; let yOff = 0;
+let inc = 5;
+
+
 function setup() {
   createCanvas(400, 400);
+
+  rectMode(CENTER);
+
+  cols = width/size;
+  rows = height/size;
+
+  for (let i = 0; i < cols; i++){
+    sizes[i] = [];
+    yOff = 0;
+    for (let j = 0; j < rows; j++){
+      sizes[i][j] = map(noise(xOff, yOff), 0, 1, 0, size);
+      yOff += inc;
+    }
+    xOff += inc;
+  }
 }
 
 function draw() {
   background(220);
+  translate(size/2, size/2);
+
+  for (let i = 0; i < cols; i++){
+    for (let j = 0; j < rows; j++){
+      rect(i*size, j*size, sizes[i][j], sizes[i][j]);
+    }
+  }
 }
