efb0b0a96911f2147d271aa853be9bac9e5c6f81
Vrinda <vrinda.bajaj@gmail.com>
2024-08-14 14:24:12 +0100

    Animate scrolling

--- 05-Perlin-Noise-1D/sketch.js
+++ 05-Perlin-Noise-1D/sketch.js
@@ -1,4 +1,4 @@
-let xOff = 0;
+let start = 0;
 let inc = 0.01;
 
 function setup() {
@@ -8,10 +8,17 @@ function setup() {
 function draw() {
   background(35);
 
+  stroke(255);
+  noFill();
+
+  beginShape();
+  let xOff = start;
+
   for(let x = 0; x < width; x++){
-    stroke(255);
     let y = noise(xOff) * height;
-    point(x, y);
+    vertex(x, y);
     xOff += inc;
   }
+  endShape();
+  start += inc;
 }
