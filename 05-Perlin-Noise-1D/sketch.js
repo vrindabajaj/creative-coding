7523f5a882fe432e94d72b175d0216b613123d37
Vrinda <vrinda.bajaj@gmail.com>
2024-08-14 14:02:59 +0100

    Display noise on screen

--- 05-Perlin-Noise-1D/sketch.js
+++ 05-Perlin-Noise-1D/sketch.js
@@ -1,7 +1,17 @@
+let xOff = 0;
+let inc = 0.01;
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(220);
+  background(35);
+
+  for(let x = 0; x < width; x++){
+    stroke(255);
+    let y = noise(xOff) * height;
+    point(x, y);
+    xOff += inc;
+  }
 }
